16af92f2c38c99fa23343d336fef2b6fa7ab4f72
gourav-uniyal <gouravuniyal17@gmail.com>
2019-08-23 19:11:00 +0530

    changed to retrofit

--- app/src/main/java/pms/co/pmsapp/libs/ApiClient.java
+++ app/src/main/java/pms/co/pmsapp/libs/ApiClient.java
@@ -1,16 +1,5 @@
 package pms.co.pmsapp.libs;
 
-import org.json.JSONObject;
-
-import java.util.HashMap;
-
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import pms.co.pmsapp.interfaces.ApiInterface;
-import pms.co.pmsapp.model.ResponseTask;
-import pms.co.pmsapp.model.Verifier;
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
